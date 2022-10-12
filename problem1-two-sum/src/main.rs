97551ce6a7015ed8a2d9e33c30c5bfb72df0c8e3
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-10-12 16:13:23 +0300

    Problem1 Added another optimized solution without using hash table.

--- problem1-two-sum/src/main.rs
+++ problem1-two-sum/src/main.rs
@@ -1,31 +1,72 @@
 use std::{collections::HashMap, fs, time::Instant, vec};
 // https://leetcode.com/problems/two-sum/
 fn main() {
-    // let _v2 = vec![3, 2, 4];
-    // let _t2 = 6;
-    // let mut _v2 = vec![2, 7, 11, 15, 17, 18, 19, 20, 21, 22, 23];
-    // let _t2 = 45;
-    // let _v2 = vec![3, 2, 3];
-    // let _t2 = 6;
-    // println!("items={:?}", _v2);
-    let _v2 = get_10000_items();
-    let _t2 = 19999;
-    let s1 = Instant::now();
-    let result = two_sum(&_v2, _t2);
-    println!("Result-1= {:?}. Elapsed time= {:?}", result, s1.elapsed());
+    let start = Instant::now();
+    // let nums = vec![2, 7, 11, 15];
+    // let target = 9;
+    // let nums = vec![3, 2, 4];
+    // let target = 6;
+    // let nums = vec![2, 7, 11, 15, 17, 18, 19, 20, 21, 22, 23, 12, 1, 11, 13, 23, 11, 22];
+    // let target = 45;
+    let nums = get_10000_items();
+    let target = 19999;
 
-    println!("--");
-    let s2 = Instant::now();
-    let result2 = two_sum_alternate(&_v2, _t2);
-    println!("Result-2= {:?}. Elapsed time= {:?}", result2, s2.elapsed());
-
-    println!("--");
-    let s3 = Instant::now();
-    let result3 = two_sum_with_hash_table(&_v2, _t2);
-    println!("Result-3= {:?}. Elapsed time= {:?}", result3, s3.elapsed());
+    // let result = two_sum_with_hash_table(nums, target);
+    let result = two_sum_without_hash_table(nums, target);
+    println!("Result= {:?}. Elapsed time= {:?}", result, start.elapsed());
 }
 
-fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+fn iterate(map: &mut HashMap<i32, (i32, i32)>, inc: usize, nums: &Vec<i32>, target: i32) -> bool {
+    for i in 0..nums.len() - inc {
+        let keyy = nums[i] + nums[i + inc];
+        let val = (i as i32, i as i32 + inc as i32);
+        map.insert(keyy, val);
+
+        if keyy == target {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn two_sum_with_hash_table(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map: HashMap<i32, (i32, i32)> = HashMap::new();
+    let mut inc: usize = 1;
+    let mut found: bool = false;
+    while !found {
+        found = iterate(&mut map, inc, &nums, target);
+        inc += 1;
+    }
+    let result = map.get(&target).unwrap();
+
+    vec![result.0, result.1]
+}
+
+fn iterate_wo(inc: usize, nums: &Vec<i32>, target: i32) -> Option<(i32, i32)> {
+    for i in 0..nums.len() - inc {
+        let keyy = nums[i] + nums[i + inc];
+        let val = (i as i32, i as i32 + inc as i32);
+
+        if keyy == target {
+            return Some(val);
+        }
+    }
+
+    return None;
+}
+
+fn two_sum_without_hash_table(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut inc: usize = 1;
+    let mut found: Option<(i32, i32)> = None;
+    while found.is_none() {
+        found = iterate_wo(inc, &nums, target);
+        inc += 1;
+    }
+    vec![found.unwrap().0, found.unwrap().1]
+}
+
+fn two_sum_first_posted_solution(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     for (p1, e1) in nums.iter().enumerate() {
         for (p2, e2) in nums.iter().enumerate() {
             if p1 == p2 {
@@ -40,55 +81,7 @@ fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     vec![0, 0]
 }
 
-fn two_sum_alternate(nums: &Vec<i32>, target: i32) -> Vec<i32> {
-    // println!("nums= {:?}, target= {}", nums, target);
-    let mut s = 0;
-    let mut e = nums.len() - 1;
-
-    while s < e {
-        let a = nums[s];
-        let b = nums[e];
-        // println!("{a}+{b}={}", a + b);
-        if a + b == target {
-            // println!("OK");
-            return vec![s as i32, e as i32];
-        }
-        e -= 1;
-        if e == s {
-            s += 1;
-            e = nums.len() - 1;
-        }
-    }
-    vec![0, 0]
-}
-
-fn call_me(map: &mut HashMap<i32, (i32, i32)>, inc: usize, nums: &Vec<i32>, target: i32) -> bool {
-    for i in 0..nums.len() - inc {
-        let keyy = nums[i] + nums[i + inc];
-        let val = (i as i32, i as i32 + inc as i32);
-        map.insert(keyy, val);
-    }
-
-    if map.get(&target).is_some() {
-        return true;
-    }
-    return false;
-}
-
-fn two_sum_with_hash_table(nums: &Vec<i32>, target: i32) -> Vec<i32> {
-    let mut map: HashMap<i32, (i32, i32)> = HashMap::new();
-    let mut inc: usize = 1;
-    let mut found: bool = false;
-    while !found {
-        found = call_me(&mut map, inc, nums, target);
-        inc += 1;
-    }
-    let rezz = map.get(&target);
-
-    vec![rezz.unwrap().0, rezz.unwrap().1]
-}
-
-pub fn two_sum_POSTED(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+fn two_sum_second_posted_solution(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     let mut s = 0;
     let limit = nums.len() - 1;
     let mut e = limit;
@@ -122,26 +115,26 @@ mod test {
 
     #[test]
     fn test_1() {
-        assert_eq!(vec![1, 2], two_sum(&vec![3, 2, 4], 6));
+        assert_eq!(vec![1, 2], two_sum(vec![3, 2, 4], 6));
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(vec![0, 1], two_sum(&vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![0, 1], two_sum(vec![2, 7, 11, 15], 9));
     }
 
     #[test]
     fn test_3() {
-        assert_eq!(vec![0, 1], two_sum(&vec![3, 3], 6));
+        assert_eq!(vec![0, 1], two_sum(vec![3, 3], 6));
     }
 
     #[test]
     fn test_4() {
-        assert_eq!(vec![0, 2], two_sum(&vec![3, 2, 3], 6));
+        assert_eq!(vec![0, 2], two_sum(vec![3, 2, 3], 6));
     }
 
     #[test]
     fn test_perf() {
-        assert_eq!(vec![9998, 9999], two_sum(&get_10000_items(), 19999));
+        assert_eq!(vec![9998, 9999], two_sum(get_10000_items(), 19999));
     }
 }
