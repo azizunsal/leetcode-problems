87588d625681ac7e1e3ea3115cb22bd99c502ef5
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-10-16 17:21:51 +0300

    Problem2 The solution optimized by eliminating type conversion.

--- problem2-add-two-numbers/src/bin/neva.rs
+++ problem2-add-two-numbers/src/bin/neva.rs
@@ -1,16 +1,15 @@
 use std::time::Instant;
 
-// #[derive(Debug, Clone)]
 #[derive(PartialEq, Eq, Clone, Debug)]
 struct ListNode {
-    val: i32,
-    next: Option<Box<ListNode>>,
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
     #[inline]
-    fn new(val: i32, next: Option<Box<ListNode>>) -> Self {
-        ListNode { val, next }
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
 }
 
@@ -18,38 +17,23 @@ fn main() {
     let start = Instant::now();
     let l1: Option<Box<ListNode>> = create_list_from_value_array(&[2, 4, 3]);
     let l2: Option<Box<ListNode>> = create_list_from_value_array(&[5, 6, 4]);
-    let l1: Option<Box<ListNode>> = create_list_from_value_array(&[9, 9, 9, 9, 9, 9, 9]);
-    let l2: Option<Box<ListNode>> = create_list_from_value_array(&[9, 9, 9, 9]);
-
     let result: Option<Box<ListNode>> = add_two_numbers(l1, l2);
     println!("result= {:?}. time= {:?}", result, start.elapsed());
 }
 
 fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let number_arr_1 = create_array_from_list(l1);
-    println!("number_arr_1= {:?}", number_arr_1);
-    let number_arr_2 = create_array_from_list(l2);
-    println!("number_arr_2= {:?}", number_arr_2);
-
-    let num1 = create_number_from_digit_array_in_rev_order(&number_arr_1);
-    let num2 = create_number_from_digit_array_in_rev_order(&number_arr_2);
-
-    let sum_of_numbers = num1 + num2;
-    println!("num1= {}, num2= {}, sum= {}", num1, num2, sum_of_numbers);
-
-    let arr: Vec<i32> = create_array_from_number(sum_of_numbers);
-
-    println!("sum_of_numbers to arr= {:?}", arr);
-
-
-    create_list_from_value_array(arr.as_slice())
+    let mut number_arr_1 = create_array_from_list(l1);
+    let mut number_arr_2 = create_array_from_list(l2);
+    let sum: Vec<i32> = sum_two_digit_arrays(&mut number_arr_1, &mut number_arr_2);
+    create_list_from_value_array(sum.as_slice())
 }
 
 fn create_list_from_value_array(values: &[i32]) -> Option<Box<ListNode>> {
-    let mut node: Option<Box<ListNode>> = Some(Box::new(ListNode::new(*values.last().unwrap(), None)));
+    let mut node: Option<Box<ListNode>> = Some(Box::new(ListNode::new(*values.last().unwrap())));
     for i in (0..values.len() - 1).rev() {
         let val = values[i];
-        let current_node = Some(Box::new(ListNode::new(val, node.clone())));
+        let mut current_node = Some(Box::new(ListNode::new(val)));
+        current_node.as_mut().map(|a| a.next = node.clone());
         node = current_node;
     }
     node
@@ -68,59 +52,90 @@ fn create_array_from_list(list: Option<Box<ListNode>>) -> Vec<i32> {
         }
         res.push(node.as_ref().unwrap().val);
     }
-
     res
 }
 
-fn create_number_from_digit_array_in_rev_order(arr: &[i32]) -> i32 {
-    let mut number_str: String = String::new();
-    for i in (0..arr.len()).rev() {
-        let digit_str: String = arr[i].to_string();
-        number_str.push(digit_str.chars().last().unwrap());
-    }
-    number_str.parse::<i32>().unwrap()
-}
+fn sum_two_digit_arrays(arr1: &mut Vec<i32>, arr2: &mut Vec<i32>) -> Vec<i32> {
+    let arr1_len = arr1.len();
+    let arr2_len = arr2.len();
 
-fn create_array_from_number(number: i32) -> Vec<i32> {
-    let mut digits: Vec<char> = number.to_string().chars().collect();
-    let mut nums: Vec<i32> = vec![-1; digits.len()];
-
-    let mut cnt: usize = digits.len() - 1;
-    for d in digits.iter_mut().rev() {
-        // nums.push(d.to_string().parse::<i32>().unwrap());
-        nums[cnt] = d.to_string().parse::<i32>().unwrap();
-        if cnt == 0 { break; }
-        cnt -= 1;
+    if arr1_len > arr2_len {
+        fix_array_length(arr2, arr1_len);
+    } else if arr2_len > arr1_len {
+        fix_array_length(arr1, arr2_len);
     }
 
-    nums.reverse();
-    let sli: &[i32] = nums.as_slice().try_into().unwrap();
-    sli.to_owned()
+    arr1.reverse();
+    arr2.reverse();
+    let mut sum_arr: Vec<i32> = Vec::with_capacity(arr1_len);
+
+    let mut carry = 0;
+    for i in (0..arr1.len()).rev() {
+        let digit1 = arr1[i];
+        let digit2 = arr2[i];
+        let mut sum = digit1 + digit2 + carry;
+        carry = if sum >= 10 { sum / 10 } else { 0 };
+        sum = if sum >= 10 { sum - 10 } else { sum };
+        println!("{} + {} = {}, carry= {}", digit1, digit2, sum, carry);
+        sum_arr.push(sum);
+    }
+
+    if carry > 0 {
+        println!("The carry (={}) must be ZERO! Adding 1 extra digit to the number.", carry);
+        sum_arr.push(carry);
+    }
+    println!("Resulting array= {:?}", sum_arr);
+    sum_arr
 }
 
+fn fix_array_length(arr: &mut Vec<i32>, limit: usize) {
+    println!("Fixing array= {:?}, limit= {}", arr, limit);
+    if arr.len() < limit {
+        for _i in 0..(limit - arr.len()) {
+            arr.push(0);
+        }
+    }
+    println!("  --> fixed array= {:?}", arr);
+}
 
 #[cfg(test)]
 mod test {
-    use crate::{add_two_numbers, create_list_from_value_array, ListNode};
+    use crate::{add_two_numbers, create_list_from_value_array, create_array_from_list, ListNode};
 
     #[test]
     fn test_1() {
         let l1: Option<Box<ListNode>> = create_list_from_value_array(&[2, 4, 3]);
         let l2: Option<Box<ListNode>> = create_list_from_value_array(&[5, 6, 4]);
-        assert_eq!(create_list_from_value_array(&[7, 0, 8]), add_two_numbers(l1, l2));
+        assert_eq!(vec![7, 0, 8], create_array_from_list(add_two_numbers(l1, l2)));
     }
 
     #[test]
     fn test_2() {
         let l1: Option<Box<ListNode>> = create_list_from_value_array(&[9, 9, 9, 9, 9, 9, 9]);
         let l2: Option<Box<ListNode>> = create_list_from_value_array(&[9, 9, 9, 9]);
-        assert_eq!(create_list_from_value_array(&[8, 9, 9, 9, 0, 0, 0, 1]), add_two_numbers(l1, l2));
+        assert_eq!(vec![8, 9, 9, 9, 0, 0, 0, 1], create_array_from_list(add_two_numbers(l1, l2)));
     }
 
     #[test]
     fn test_3() {
         let l1: Option<Box<ListNode>> = create_list_from_value_array(&[0]);
         let l2: Option<Box<ListNode>> = create_list_from_value_array(&[0]);
-        assert_eq!(create_list_from_value_array(&[0]), add_two_numbers(l1, l2));
+        assert_eq!(vec![0], create_array_from_list(add_two_numbers(l1, l2)));
+    }
+
+    #[test]
+    fn test_4() {
+        let l1: Option<Box<ListNode>> = create_list_from_value_array(&[9]);
+        let l2: Option<Box<ListNode>> = create_list_from_value_array(&[1, 9, 9, 9, 9, 9, 9, 9, 9, 9]);
+        assert_eq!(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], create_array_from_list(add_two_numbers(l1, l2)));
+    }
+
+    #[test]
+    fn test_5() {
+        let l1: Option<Box<ListNode>> = create_list_from_value_array(&[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+        let l2: Option<Box<ListNode>> = create_list_from_value_array(&[5, 6, 4]);
+        let mut res: Vec<i32> = vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 6];
+        res.reverse();
+        assert_eq!(res, create_array_from_list(add_two_numbers(l1, l2)));
     }
 }
